936e8b84576c8b19d225d60158a0ced16a2d841f
Francis-Rockwell <yzr21@mails.tsinghua.edu.cn>
2024-06-22 02:59:02 +0800

    model_type

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import argparse
 import pandas as pd
 from data.pre_process import Dataset
 from models.model import Model
 from models import logistic_regression, support_vector_machine, neural_network
 
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-M",
+        "--model_type",
+        type=str,
+        required=True,
+        choices=["LR", "SVM", "NN"],
+    )
+    args = parser.parse_args()
+
     train_data_public = pd.read_csv("data/train_public.csv")
     train_data_internet = pd.read_csv("data/train_internet.csv")
     test_data_public = pd.read_csv("data/test_public.csv")
@@ -16,29 +28,27 @@ if __name__ == "__main__":
 
     dataset.preprocess()
 
-    logisticRegression = logistic_regression.LogisticRegression(
-        feature=dataset.train_feature, label=dataset.train_label
-    )
-    supportVectorMachine = support_vector_machine.SupportVectorMachine(
-        feature=dataset.train_feature, label=dataset.train_label
-    )
-    neuralNetwork = neural_network.NeuralNetwork(
-        feature=dataset.train_feature, label=dataset.train_label
-    )
-
-    # model_prefix = ["logistic_regression", "support_vector_machine", "neural_network"]
-    model_prefix = ["logistic_regression", "support_vector_machine"]
-
-    # models: list[Model] = [logisticRegression, supportVectorMachine, neuralNetwork]
-    models: list[Model] = [logisticRegression, supportVectorMachine]
-
-    for index, model in enumerate(models):
-        model.train()
-        print(f"{model_prefix[index]} validation roc: {model.validate()}")
-        result = pd.DataFrame(
-            {
-                "id": dataset.test_id,
-                "isDefault": model.predict(dataset.test_feature),
-            }
+    if args.model_type == "LR":
+        model = logistic_regression.LogisticRegression(
+            feature=dataset.train_feature, label=dataset.train_label
         )
-        result.to_csv(f"{model_prefix[index]}_submission.csv")
+        prefix = "logistic_regression"
+    elif args.model_type == "SVM":
+        model = support_vector_machine.SupportVectorMachine(
+            feature=dataset.train_feature, label=dataset.train_label
+        )
+        prefix = "support_vector_machine"
+    elif args.model_type == "NN":
+        model = neuralNetwork = neural_network.NeuralNetwork(
+            feature=dataset.train_feature, label=dataset.train_label
+        )
+        prefix = "neural_network"
+
+    model.train()
+    print(f"{prefix} validation roc: {model.validate()}")
+    pd.DataFrame(
+        {
+            "id": dataset.test_id,
+            "isDefault": model.predict(dataset.test_feature),
+        }
+    ).to_csv(f"{prefix}_submission.csv")
