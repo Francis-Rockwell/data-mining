c31f4f24e9d46d3e5c253f7bd8f5fd6af54f128c
Francis-Rockwell <yzr21@mails.tsinghua.edu.cn>
2024-06-22 19:39:31 +0800

    deepcopy

--- models/neural_network.py
+++ models/neural_network.py
@@ -4,6 +4,8 @@ import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
+from sklearn.metrics import roc_auc_score
+import copy
 
 
 class NeuralNetwork(Model):
@@ -26,16 +28,17 @@ class NeuralNetwork(Model):
                 torch.Tensor(self.evaluate_feature),
                 torch.Tensor(np.array(self.evaluate_label)),
             ),
-            batch_size=100,
-            shuffle=True,
+            batch_size=self.evaluate_feature.shape[0],
+            shuffle=False,
         )
         self.criterion = torch.nn.BCELoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        self.path = "best_model.pth"
 
     def train(self):
-        max_epoches = 100
-        early_stop_patience = 5
-        best_val_loss = float("inf")
+        max_epoches = 1000
+        early_stop_patience = 10
+        best_val_auc = 0.0
         self.best_model = None
 
         for epoch in range(max_epoches):
@@ -52,28 +55,37 @@ class NeuralNetwork(Model):
 
             self.model.eval()
             total_val_loss = 0.0
+            total_val_auc = 0.0
             with torch.no_grad():
                 for batch_data, batch_targets in self.evaluate_loader:
                     outputs = self.model(batch_data)
                     loss = self.criterion(outputs, batch_targets.unsqueeze(1).float())
+                    val_auc = roc_auc_score(
+                        batch_targets.unsqueeze(1).float(), outputs.squeeze().detach()
+                    )
                     total_val_loss += loss.item()
+                    total_val_auc += val_auc
                 avg_val_loss = total_val_loss / len(self.evaluate_loader)
+                avg_val_auc = total_val_auc / len(self.evaluate_loader)
 
-            print(
-                f"Epoch [{epoch+1}/{max_epoches}], Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}"
-            )
+                print(
+                    f"Epoch [{epoch+1}/{max_epoches}], Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Val AUC: {avg_val_auc:.4f}"
+                )
 
-            if avg_val_loss < best_val_loss:
-                best_val_loss = avg_val_loss
-                current_patience = 0
-                self.best_model = self.model.state_dict()
-            else:
-                current_patience += 1
-                if current_patience >= early_stop_patience:
-                    print(f"Early stopping at epoch {epoch+1}")
-                    break
+                if avg_val_auc > best_val_auc:
+                    best_val_auc = avg_val_auc
+                    current_patience = 0
+                    self.best_model = copy.deepcopy(self.model.state_dict())
+                else:
+                    current_patience += 1
+                    if current_patience >= early_stop_patience:
+                        print(f"Early stopping at epoch {epoch+1}")
+                        break
+
+        torch.save(self.best_model, self.path)
 
     def predict(self, feature):
         self.model.load_state_dict(self.best_model)
+        # self.model.load_state_dict(torch.load(self.path))
         self.model.eval()
         return self.model(torch.Tensor(feature)).squeeze().detach()
--- models/neural_networks/mlp.py
+++ models/neural_networks/mlp.py
@@ -6,16 +6,21 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         self.input_size = input_size
         self.output_size = output_size
-        self.hidden_size = 2 * self.input_size
+        self.hidden_size = [2 * self.input_size, self.input_size]
 
-        self.fc1 = nn.Linear(self.input_size, self.hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(self.hidden_size, self.output_size)
+        self.fc1 = nn.Linear(self.input_size, self.hidden_size[0])
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(self.hidden_size[0], self.output_size)
+        self.fc2 = nn.Linear(self.hidden_size[0], self.hidden_size[1])
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(self.hidden_size[1], self.output_size)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.relu(x)
+        x = self.relu1(x)
         x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
         x = self.sigmoid(x)
         return x
